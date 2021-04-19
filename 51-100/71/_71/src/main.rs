fdac77aa5e26bc1498261af7fa6da1f58126c81e
Gvstavo <gustavopaivads@gmail.com>
2021-04-18 23:00:44 -0300

    71 using farey series failed

--- 51-100/71/_71/src/main.rs
+++ 51-100/71/_71/src/main.rs
@@ -1,27 +1,54 @@
 mod frac;
 use frac::Frac;
 
-fn phi_1_to_n(n: i64) -> Vec<i64> {
+/*
 
-	let mut phi : Vec<i64> = (0..=n).collect();
+    (a, b, c, d) = (0, 1, 1, n)
+    if descending:
+        (a, c) = (1, n - 1)
+    print("{0}/{1}".format(a, b))
+    while (c <= n and not descending) or (a > 0 and descending):
+        k = (n + b) // d
+        (a, b, c, d) = (c, d, k * c - a, k * d - b)
+        print("{0}/{1}".format(a, b))
 
-	let mut ret : Vec<Frac> = Vec::new();
+*/
 
-	for i in 2..=n{
+fn _farey(n: i64) -> (i64, i64) {
 
-		if phi[i as usize] == i {
-			for j in (i..=n).step_by(i as usize)  {
-				phi[j as usize] -= phi[j as usize].checked_div(i).unwrap();
-			}
-		}
-	};
+	let  (mut a,mut  b, mut c, mut d) = (0, 1, 1, n);
 
-	phi
+	let mut aux =  (0, 1, 1, n);
+	let mut count = 0;
+	let mut k = 0;	
+	let mut prev  = (a,b);
+	while c <= n {
+
+		k = (n + b).div_euclid(d);
+		count  += 1;
+		println!("{:?}",count );
+		aux = (c, d, k * c - a, k * d - b);
+
+		prev = (a,b);
+
+
+		a = aux.0;
+		b = aux.1;
+		c = aux.2;
+		d = aux.3;
+		
+		if (a,b) == (3,7){
+			break
+		}	
+	}
+	prev
+
 }
 
 
 fn main() {
-	phi_1_to_n(12000);
-	// print!("{:?}", phi_1_to_n(10));
+	
+	let ret = _farey(1_000_000);	
 
+	println!("{:?}",ret );
 }

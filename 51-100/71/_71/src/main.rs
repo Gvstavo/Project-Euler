6f8c5cc5f84d7731454a021de6f9424149add560
Gvstavo <gustavopaivads@gmail.com>
2021-04-21 13:28:45 -0300

    > 71, too slow

--- 51-100/71/_71/src/main.rs
+++ 51-100/71/_71/src/main.rs
@@ -1,54 +1,48 @@
-mod frac;
-use frac::Frac;
+fn count_sb(n: i64) -> i64{
 
-/*
+	let limit =n;
 
-    (a, b, c, d) = (0, 1, 1, n)
-    if descending:
-        (a, c) = (1, n - 1)
-    print("{0}/{1}".format(a, b))
-    while (c <= n and not descending) or (a > 0 and descending):
-        k = (n + b) // d
-        (a, b, c, d) = (c, d, k * c - a, k * d - b)
-        print("{0}/{1}".format(a, b))
+	let mut count : i64 = 0;
+	let mut top : i64 = 0;
+	let mut stack : Vec<i64> = vec![0; limit.div_euclid(2) as usize];
 
-*/
+	// stack.reserve(limit.div_euclid(2) as usize);
 
-fn _farey(n: i64) -> (i64, i64) {
+	let mut left : i64= 3;
 
-	let  (mut a,mut  b, mut c, mut d) = (0, 1, 1, n);
+	let mut right : i64= 2;
 
-	let mut aux =  (0, 1, 1, n);
-	let mut count = 0;
-	let mut k = 0;	
-	let mut prev  = (a,b);
-	while c <= n {
-
-		k = (n + b).div_euclid(d);
-		count  += 1;
-		println!("{:?}",count );
-		aux = (c, d, k * c - a, k * d - b);
-
-		prev = (a,b);
-
-
-		a = aux.0;
-		b = aux.1;
-		c = aux.2;
-		d = aux.3;
+	loop {
 		
-		if (a,b) == (3,7){
-			break
-		}	
+		let med = left + right;
+		if med > limit {
+			if top > 0{
+				left = right;
+				top = top -1;
+				right = stack[top as usize];
+			}
+			else{
+				break;
+			}
+		}
+		else{
+			count += 1;
+			stack[top as usize] = right;
+			top +=1;
+			right = med;
+		}
 	}
-	prev
+
+	count
+
 
 }
 
 
 fn main() {
 	
-	let ret = _farey(1_000_000);	
+	let ret = count_sb(1_000_000);
 
-	println!("{:?}",ret );
+	println!("{:?}", ret);
+
 }

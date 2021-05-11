cc98b366b7dc049973777ca7b4d81103d6e54d37
Gvstavo <gustavopaivads@gmail.com>
2021-05-10 23:21:50 -0300

    > new structs

--- 51-100/54/_54/src/main.rs
+++ 51-100/54/_54/src/main.rs
@@ -1,8 +1,12 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+
 #[derive(Debug)]
-enum Hand {
+enum Ranking {
 	RoyalFlush,
 	StraightFlush,
 	FourKind,
@@ -15,6 +19,27 @@ enum Hand {
 	HighCard
 }
 
+#[derive(Debug)]
+enum Winner{
+	Player1,
+	Player2
+}
+
+#[derive(Debug)]
+struct Card {
+	value: String,
+	suit: String
+}
+
+
+#[derive(Debug)]
+struct Player{
+	hand: Vec<Card>
+	//hand: (Card , Card , Card, Card , Card)
+}
+
+
+
 fn main() {
  
 	let file = File::open("p054_poker.txt").unwrap();
@@ -24,6 +49,22 @@ fn main() {
 
   let hands : Vec<(&str, &str)> = contents.split("\n").filter_map(|x| if x.len() == 29 { Some(x.split_at(14))} else {None}).collect();
 
-  println!("{:?}", hands);
+
+}
+
+#[test]
+fn is_royal_flush() {
+
+	let h = Player{
+		hand: vec![
+			Card{value: "A".to_string(), suit: "H".to_string()},
+			Card{value: "K".to_string(), suit: "H".to_string()},
+			Card{value: "Q".to_string(), suit: "H".to_string()},
+			Card{value: "J".to_string(), suit: "H".to_string()},
+			Card{value: "10".to_string(), suit: "H".to_string()},
+		]
+	};
+
+	
 
 }

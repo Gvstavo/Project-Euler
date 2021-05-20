424179b783acbde702a3ca725d59c574dd597545
Gvstavo <gustavopaivads@gmail.com>
2021-05-19 22:39:17 -0300

    > 54 geral compara

--- 51-100/54.exs
+++ 51-100/54.exs
@@ -16,7 +16,23 @@ defmodule Poker do
 
 	end
 
+	defp value(hand) when hand == :royal_flush, do: 10
+	defp value(hand) when hand == :straight_flush, do: 9
+	defp value(hand) when hand == :four_of_a_kind, do: 8
+	defp value(hand) when hand == :full_house, do: 7
+	defp value(hand) when hand == :flush, do: 6
+	defp value(hand) when hand == :straight, do: 5
+	defp value(hand) when hand == :three_of_a_kind, do: 4
+	defp value(hand) when hand == :two_pair, do: 3
+	defp value(hand) when hand == :pair, do: 2
+	defp value(_hand) , do: 1
 
+
+
+	def comparator({h1,_} , {h2,_}) when h1 != h2 do
+		if value(h1) > value(h2), do: :player1, else: :player2
+	end
+		
 
 	defp is_sequence(values) do
 
@@ -25,6 +41,7 @@ defmodule Poker do
 
 		String.match?(Poker.card , regex)
 	end
+
 
 	def ranking(hand) do
 		case hand do
@@ -63,13 +80,13 @@ defmodule Poker do
 
 							[v1 , s1 , v2 , s2 , v5,_,v3 , s3 , v4 , s4] when v1 == v2 and s1 != s2 and v3 == v4 and s3 != s4 ->  {:two_pair, [v2 , v1, v5]}
 
-							[v1 , s1 , v2 , s2 , v3 , _, v4 , _ , v5, _]   when v1 == v2 and s1 != s2 -> {:pair, Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
+							[v1 , s1 , v2 , s2 , v3 , _, v4 , _ , v5, _]   when v1 == v2 and s1 != s2 -> {:pair, [v1] ++ Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
 
-							[v5 , _, v4 , _ , v3, _, v1 , s1 , v2 , s2]   when v1 == v2 and s1 != s2 ->{:pair, Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
+							[v5 , _, v4 , _ , v3, _, v1 , s1 , v2 , s2]   when v1 == v2 and s1 != s2 ->{:pair, [v1] ++ Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
 
-							[v5 , _, v1 , s1 , v2 , s2,v3 , _ , v4, _, ]   when v1 == v2 and s1 != s2 ->{:pair, Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
+							[v5 , _, v1 , s1 , v2 , s2,v3 , _ , v4, _, ]   when v1 == v2 and s1 != s2 ->{:pair, [v1] ++ Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
 
-							[v5, _,  v4, _, v1 , s1 , v2 , s2,v3 , _ ]   when v1 == v2 and s1 != s2 ->{:pair, Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
+							[v5, _,  v4, _, v1 , s1 , v2 , s2,v3 , _ ]   when v1 == v2 and s1 != s2 ->{:pair,[v1] ++  Enum.sort_by([v3,v4,v5],&Poker.sorter/1)}
 
 							[v1 , _ , v2 , _ , v3 , _ , v4 , _ , v5, _]  -> {:highest_card, Enum.sort_by([v1,v2,v3,v4,v5],&Poker.sorter/1)}
 
@@ -110,9 +127,73 @@ txt
 
 end)
 |> Enum.map(fn [a, b] -> {Poker.ranking(a),Poker.ranking(b)}end)
-|> Enum.filter(fn {a,b} -> a == b end)
+|> Enum.reject(fn {{a,_},{b,_}} -> a == b end)
+|> Enum.count(fn  {a,b} -> Poker.comparator(a,b) == :player1 end)
+|> IO.inspect
 
 #Poker.ranking(["5","H" ,"5","C","6","S","7","S", "K","D"]) |> IO.inspect
+
+"2C 3S 8S 8D TD"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+
+"5D 8C 9S JS AC"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+
+
+"4D 6S 9H QH QC"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+
+"3D 6D 7H QD QS"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+
+"2H 2D 4C 4D 4S"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+
+"3C 3D 3S 9S 9D"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+
+
+h1 = "2D 9C AS AH AC"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+#|> IO.inspect
+
+h2 = "3D 6D 7D TD QD"
+|> String.split(" ", trim: true)
+|> Enum.sort_by(&Poker.sorter/1)
+|> Enum.join
+|> String.split("",trim: true)
+|> Poker.ranking
+#|> IO.inspect
+
+Poker.comparator(h1,h2)# |> IO.inspect
 # Poker.ranking(["A","D","K","D","Q","D","J","D","T","D"])
 # |> IO.inspect
 
